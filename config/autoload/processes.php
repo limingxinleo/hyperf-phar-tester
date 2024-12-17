8b1be03b84db73c587e2bdb6c2edbc056bea97dd
李铭昕 <715557344@qq.com>
2024-12-17 17:39:19 +0800

    Format

--- config/autoload/processes.php
+++ config/autoload/processes.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 /**
  * This file is part of Hyperf.
  *
@@ -10,8 +9,5 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-
-use Hyperf\AsyncQueue\Process\ConsumerProcess;
-
 return [
 ];
